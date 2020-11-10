7de00e3524d8bd14bb46e2d637f07637beeb8d76
daniel tea <daniel.l.tea@gmail.com>
2020-11-09 20:09:25 -0800

    add mocha example

---
+++ src/complex-calculator.mocha.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+
+const sumStub = sinon.stub()
+const complexCalculator = proxyquire('./complex-calculator', (
+  { './sum': sumStub }
+))
+
+describe('complexCalculator', () => {
+  it('calculates something complex', () => {
+    sumStub.returns(42)
+
+    assert.equal(complexCalculator(1, 2), 42)
+    assert.equal(sumStub.withArgs(1, 2).callCount, 1)
+  })
+})
